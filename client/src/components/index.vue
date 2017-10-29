674fe05861d7d41a52fc2d95af1d4967debc087b
3kynox <thierry.prost@gmail.com>
2017-10-29 23:51:29 +0100

    BUILD THE CLIENT: landing page

---
+++ client/src/components/index.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="index-page bg-grey-10 window-height window-width column items-center no-wrap">
+    <div class="banner bg-blue-grey-10 flex flex-center">
+      <img src="~assets/gb-logo-standalone.png">
+    </div>
+    <div class="text-center">
+      <div class="card bg-grey-10 shadow-4 column no-wrap flex-center group">
+        <img src="~assets/gb-logo.png">
+        <q-btn
+          color="cyan-10"
+          push
+          @click="$router.push('/dashboard')"
+          class="full-width"
+          icon="layers"
+        >
+          Go to Dashboard
+        </q-btn>
+
+        <q-btn
+          color="secondary"
+          push
+          class="full-width"
+          @click="$router.push('/config')"
+          icon="launch"
+        >
+          Configure Gunbot
+        </q-btn>
+
+        <a
+          class="row flex-center text-secondary"
+          style="margin-top: 15px; font-size: .75rem"
+        >
+          <div>Privacy Policy</div>
+        </a>
+      </div>
+    </div>
+    <a class="ribbon" href="https://github.com/GuntharDeNiro/BTCT/tree/saas-dev" :title="`Fork Project on Github`"></a>
+  </div>
+</template>
+
+<script>
+import { QBtn } from 'quasar'
+
+export default {
+  components: {
+    QBtn
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="styl">
+.index-page
+  .banner
+    height 50vh
+    width 100%
+    font-size 30vmax
+    color rgba(255, 255, 255, .2)
+    overflow hidden
+    img
+      width 250px
+  .card
+    width 80vw
+    max-width 500px
+    padding 10px 25px
+    margin-top -90px
+    border-radius 2px
+    img
+      height 180px
+
+.ribbon
+  width 12.1em
+  height 12.1em
+  position absolute
+  overflow hidden
+  top 0
+  right 0
+  z-index 39
+  pointer-events none
+  font-size 15px
+  text-decoration none
+  text-indent -999999px
+  opacity 0.7
+  &.fixed
+    position fixed
+  &:before
+    content ''
+    padding .38em 0
+    background-color white
+    background-image linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, .15))
+    box-shadow 0 .15em .23em 0 rgba(0, 0, 0, .5)
+    pointer-events auto
+  &:after
+    content attr(title)
+    color #424242
+    line-height 1.54em
+    text-decoration none
+    text-align center
+    text-indent 0
+    padding .15em 0
+    margin .15em 0
+
+  &:before, &:after
+    position absolute
+    display block
+    width 15.38em
+    height 1.74em
+    top 3.23em
+    right -3.23em
+    transform rotate(45deg)
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -7,8 +7,8 @@ require(`quasar/dist/quasar.${__THEME}.css`)
 // ==============================
 
 // Uncomment the following lines if you need IE11/Edge support
-// require(`quasar/dist/quasar.ie`)
-// require(`quasar/dist/quasar.ie.${__THEME}.css`)
+require(`quasar/dist/quasar.ie`)
+require(`quasar/dist/quasar.ie.${__THEME}.css`)
 
 import Vue from 'vue'
 import Quasar from 'quasar'
@@ -18,12 +18,12 @@ Vue.config.productionTip = false
 Vue.use(Quasar) // Install Quasar Framework
 
 if (__THEME === 'mat') {
-  // require('quasar-extras/roboto-font')
+  require('quasar-extras/roboto-font')
 }
-// import 'quasar-extras/material-icons'
+import 'quasar-extras/material-icons'
 // import 'quasar-extras/ionicons'
 // import 'quasar-extras/fontawesome'
-// import 'quasar-extras/animate'
+import 'quasar-extras/animate'
 
 /* eslint-disable no-new */
 new Vue({
